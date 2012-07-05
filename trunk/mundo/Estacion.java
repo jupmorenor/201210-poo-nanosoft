73799ceb0477014cc80c072c2f4c9a3a7830d362
darkknight64@gmail.com <darkknight64@gmail.com@fdbdeb93-9192-0c64-534a-bb3bff6a2a14>
2012-07-05 05:40:23 +0000

    se agregan comentarios

--- trunk/mundo/Estacion.java
+++ trunk/mundo/Estacion.java
@@ -1,13 +1,42 @@
+/**
+ * -----------------------------------------------------------------
+ * Universidad Distrital Francisco Jose de Caldas
+ * Facultad de Ingenieria
+ * Proyecto curricular de Ingenieria de Sistemas
+ *
+ * Proyecto final de Programacion Orientada a Objetos
+ * Presentado por:
+ * Juan Pablo Moreno Rico - codigo: 20111020059
+ * Andrés Felipe Rodriguez Rincón - codigo: 20112020034
+ * Bajo el nombre de "NanoSoft - Gestion de Usuarios SitUD"
+ * -----------------------------------------------------------------
+
+/**
+ * 
+ */
+
+
 package ud.poo.mundo;
+/**
+ * Clase que representa una estación
+ */
 
 public class Estacion {
 String idEstacion;
-
+/**
+ * Crea una nueva estacion <br>
+ *  Asigna a la id de la estacion el id de la ruta
+ */
 Estacion(String idRuta){
-	
+	idEstacion=idRuta;
 }
+/**
+ * Devuelve el id de la estacion
+ * @return idEstacion
+ */
 
 public String daridEstacion(){
-	
+return idEstacion;	
+}
 }
 }
